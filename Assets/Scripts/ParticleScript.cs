292aa780a0c394ecdedafac684269c3273d6acea
Pavlos Ts <tsikrikaspavlos@gmail.com>
2024-01-10 18:36:37 +0200

    Update stage, added fixes

--- Assets/Scripts/ParticleScript.cs
+++ Assets/Scripts/ParticleScript.cs
@@ -5,6 +5,8 @@ public class ParticleScript : MonoBehaviour
 {
     public ParticleSystem particleSystem;
     public Collider fireCollider; // Add this line
+    public AudioSource audioSource; // Add this line for the audio source
+    public AudioClip audioClip; // Add this line for the audio clip
 
     private void Start()
     {
@@ -17,9 +19,11 @@ public class ParticleScript : MonoBehaviour
         {
             particleSystem.Play();
             fireCollider.enabled = true; // Enable the collider
+            audioSource.PlayOneShot(audioClip); // Play the audio clip
             yield return new WaitForSeconds(4);
             particleSystem.Stop();
             fireCollider.enabled = false; // Disable the collider
+            audioSource.Stop(); // Stop the audio
             yield return new WaitForSeconds(3);
         }
     }
